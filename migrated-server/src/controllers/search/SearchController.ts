10b7a763c2266af2b23418c3239a43ac9611da19
Agarvv <casluagarv@gmail.com>
2026-06-27 12:27:01 +0300

    issues

--- migrated-server/src/controllers/search/SearchController.ts
+++ migrated-server/src/controllers/search/SearchController.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from 'express'
 import SearchService from '@services/search/SearchService'
+//    
 
+// 
+
+//
 class SearchController {
     public static async search(req: Request, res: Response) {
         const { query } = req.params;
