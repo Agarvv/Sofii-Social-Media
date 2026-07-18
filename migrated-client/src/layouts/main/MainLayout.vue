209f32820c652a265371180d4d0e50e98534d377
Agarv <162643502+Agarvv@users.noreply.github.com>
2026-07-18 23:51:06 +0000

    issues

--- migrated-client/src/layouts/main/MainLayout.vue
+++ migrated-client/src/layouts/main/MainLayout.vue
@@ -10,12 +10,12 @@
   
   <script lang="ts">
    import { defineComponent } from 'vue'
-   import HeaderComponent from '@/shared/header/HeaderComponent.vue';
+   //import HeaderComponent from '@/shared/header/HeaderComponent.vue';
   
    export default defineComponent({
       name: 'MainLayout',
       components: {
-          HeaderComponent, 
+     //     HeaderComponent, 
       },
    });
   </script>
